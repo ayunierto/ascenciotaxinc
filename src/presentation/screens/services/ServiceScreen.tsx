d4329f2d8d348143eaa8a124032ae6708480ca93
Alcides Turruellas <ayunierto@gmail.com>
2024-10-31 02:17:31 -0500

    created screens

---
+++ src/presentation/screens/services/ServiceScreen.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+
+export default class ServiceScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>ServiceScreen</Text>
+      </View>
+    );
+  }
+}
